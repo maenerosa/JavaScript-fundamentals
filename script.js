12484c6156551934fe6c2fdbdea868dfc1d55d84
kodego-mae <funj451@gmail.com>
2023-03-22 19:50:05 +0700

    update files

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const button = document.querySelector("button");
+let grade = prompt("What is your grade?");
 
-function greet() {
-  const name = "Mae";
-  console.log(name);
+switch (true) {
+  case grade >= 90:
+    console.log("A");
+    break;
+
+  case grade >= 80:
+    console.log("B");
+    break;
+
+  case grade >= 70:
+    console.log("C");
+    break;
+  case grade >= 60:
+    console.log("D");
+    break;
+  default:
+    console.log("F");
 }
-
-button.addEventListener();
